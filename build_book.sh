56865cce50cd94289ba9fcf2bac227dd7fa3dbaa
Ethan Weed <weed.ethan@gmail.com>
2021-10-12 22:05:37 +0200

    repaced old yaml files with new

--- build_book.sh
+++ build_book.sh
@@ -2,7 +2,7 @@
 
 
 # build html documents
-jupyter-book build /Users/ethan/Documents/GitHub/ExPsyLing/2021/Book/ --path-output /Users/ethan/Documents/GitHub/ExPsyLing/2021/Book --config /Users/ethan/Documents/GitHub/ExPsyLing/2021/Book/yaml/_config.yml --toc /Users/ethan/Documents/GitHub/ExPsyLing/2021/Book/yaml/_toc.yml
+jupyter-book build /Users/ethan/Documents/GitHub/ExPsyLingExtras/Book --path-output /Users/ethan/Documents/GitHub/ExPsyLingExtras/Book --config /Users/ethan/Documents/GitHub/ExPsyLingExtras/Book/yaml/_config.yml --toc /Users/ethan/Documents/GitHub/ExPsyLingExtras/Book/yaml/_toc.yml
 
 
-ghp-import -n -p -f Book/_build/html
+# ghp-import -n -p -f Book/_build/html
